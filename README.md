3100db80dcccb1c11d32f4521d771c51e1e3ffee
Samuel Wright <wsright987@gmail.com>
2023-02-02 22:25:11 -0500

    Readme updated

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
-# chat-plus
- 
+# ChatPlus Livestream Chat Extension for Rumble.com
+
+This Chrome extension adds livestream chat features to Rumble.com to enhance your viewing experience.
+
+## Features
+- Colorized usernames 
+- User mention highlighting
+- View list users who've chatted
+
+## Installation
+- Download the extension's files. 
+- From the chrome extensions page, turn on "Developer Mode" and click "Load unpacked" to add the extension.
+- You will see the extension icon in your browser's toolbar. 
+
+## Usage
+- Login and open a livestream on Rumble.com.
+- Mentions with your username will be highlighted and usernames will be colorized.
+- Type "@" to view the user list. Click name to add to message input.
+- Click the extension icon in the browser's toolbar to open the options.
+
+## Support
+- For support and troubleshooting, please contact me at wsright987@gmail.com.
+
+## Contributing
+- If you would like to contribute to the development of this extension, please fork the repository and submit a pull request.
--- scripts/content.js
+++ scripts/content.js
@@ -5,16 +5,9 @@
   1. Add option to turn off username colors
 
   2. Add popup to show list of usernames in chat
-    - If @ is pressed, show list of usernames in chat. 
-    - If username is selected, add username to message
-    - add new users to list as they join chat
-    - remove users from list periodically
-
-  3. Find all username mentions
+    - autocomplete usernames when typing '@' in chat input
 
 */
-
-
 
 
 // Store chat history
@@ -384,15 +377,16 @@ document.addEventListener("click", function(event) {
 
 // Refresh chat history every 120 seconds
 const chatRefreshInterval = setInterval(function(){
-  console.log('refreshing chat history');
+  //console.log('refreshing chat history');
   getChatHistory()
-}, 12000);
+}, 120000);
 
  // Clear interval if there is no chat history
 if (!chatHistoryList){
-  console.log('clearing chat refresh interval')
+  //console.log('clearing chat refresh interval')
   clearInterval(chatRefreshInterval);
 }
+
 
 
 
@@ -421,4 +415,76 @@ testBtn.addEventListener('click', ()=>{
 });
 //chatHistoryEle[0].appendChild(testBtn);
 
+
+
+
+
+
+/*
+const autocomplete = (input, arr) => {  
+  let currentFocus;
+  input.addEventListener("input", function(e) {
+    let a, b, i, val = this.value;
+    closeAllLists();
+    if (!val) { return false;}
+    currentFocus = -1;
+    a = document.createElement("DIV");
+    a.setAttribute("id", this.id + "autocomplete-list");
+    a.setAttribute("class", "autocomplete-items");
+    this.parentNode.appendChild(a);
+    for (i = 0; i < arr.length; i++) {
+      if (arr[i].substr(0, val.length).toUpperCase() == val.toUpperCase()) {
+        b = document.createElement("DIV");
+        b.innerHTML = "<strong>" + arr[i].substr(0, val.length) + "</strong>";
+        b.innerHTML += arr[i].substr(val.length);
+        b.innerHTML += "<input type='hidden' value='" + arr[i] + "'>";
+        b.addEventListener("click", function(e) {
+          input.value = this.getElementsByTagName("input")[0].value;
+          closeAllLists();
+        });
+        a.appendChild(b);
+      }
+    }
+  });
+  input.addEventListener("keydown", function(e) {
+    let x = document.getElementById(this.id + "autocomplete-list");
+    if (x) x = x.getElementsByTagName("div");
+    if (e.keyCode == 40) {
+      currentFocus++;
+      addActive(x);
+    } else if (e.keyCode == 38) {
+      currentFocus--;
+      addActive(x);
+    } else if (e.keyCode == 13) {
+      e.preventDefault();
+      if (currentFocus > -1) {
+        if (x) x[currentFocus].click();
+      }
+    }
+  });
+  function addActive(x) {
+    if (!x) return false;
+    removeActive(x);
+    if (currentFocus >= x.length) currentFocus = 0;
+    if (currentFocus < 0) currentFocus = (x.length - 1);
+    x[currentFocus].classList.add("autocomplete-active");
+  }
+  function removeActive(x) {
+    for (let i = 0; i < x.length; i++) {
+      x[i].classList.remove("autocomplete-active");
+    }
+  }
+  function closeAllLists(elmnt) {
+    let x = document.getElementsByClassName("autocomplete-items");
+    for (let i = 0; i < x.length; i++) {
+      if (elmnt != x[i] && elmnt != input) {
+        x[i].parentNode.removeChild(x[i]);
+      }
+    }
+  }
+  document.addEventListener("click", function (e) {
+    closeAllLists(e.target);
+  });
+}
+*/
 

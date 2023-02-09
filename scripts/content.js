096338974a6b7ea10064c7f25f957910bb681aea
Samuel Wright <wsright987@gmail.com>
2023-02-09 15:34:43 -0500

    Refresh user count interval added

--- scripts/content.js
+++ scripts/content.js
@@ -1235,10 +1235,20 @@ var setIntervals = function() {
         showListUserCount 
           ? `<span>${getUserCount(userColors)}</span>`
           : `<svg xmlns="http://www.w3.org/2000/svg" width="10" height="10" fill="currentColor" class="bi bi-arrow-clockwise" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M8 3a5 5 0 1 0 4.546 2.914.5.5 0 0 1 .908-.417A6 6 0 1 1 8 2v1z"/><path d="M8 4.466V.534a.25.25 0 0 1 .41-.192l2.36 1.966c.12.1.12.284 0 .384L8.41 4.658A.25.25 0 0 1 8 4.466z"/></svg>`
-      );
+      );   
+    }
 
-      
-      if (enableUsernameMenu && document.querySelector('.username-menu-list')) buildUsernameList(false);
+    // Refresh user list 
+    if (
+      enableUsernameMenu 
+      && document.querySelector('.username-menu-list')
+    ) {
+      buildUsernameList(false);
+    }
+
+    // Refresh user list count if enabled
+    if (showListUserCount) {
+      document.querySelector('.username-menu-refresh-button').innerHTML = `<span style="width: fit-content;">${getUserCount(userColors)}</span>`;
     }
 
     // Clear interval if there is no chat history

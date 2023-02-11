e9533438dd9d96a64d84562da9bd92480188b67a
Samuel Wright <wsright987@gmail.com>
2023-02-11 01:40:04 -0500

    Removed test function

--- scripts/content.js
+++ scripts/content.js
@@ -1325,21 +1325,3 @@ var setIntervals = function() {
     clearInterval(chatRefreshInterval);
   }
 }
-
-
-//////   Background 2/11/2023   //////
-
-//////   Testing   //////
-
-// Example of a simple user data object
-const user = {
-  username: 'demo-user'
-};
-
-chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-  // 2. A page requested user data, respond with a copy of `user`
-  if (message === 'get-user-data') {
-    sendResponse(user);
-  }
-
-});
